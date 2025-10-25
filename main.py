7c2201554276f773c21ea050ff6128525689aa4f
Peter Oyerinde <peteroluwa2019@gmail.com>
2025-10-25 19:44:24 +0100

    Add requirements.txt for Render deployment

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from routes import devotional, events, admin, auth
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
+import uvicorn
 
 app = FastAPI()
 
@@ -18,9 +19,14 @@ app.add_middleware(
     allow_methods = ["*"]
 )
 
+
+
 app.include_router(devotional.devotional_router, tags=["Devotional"], prefix="/devotional")
 app.include_router(events.events_router, tags=["events"], prefix="/events")
 app.include_router(admin.admin_router, tags=["admin"], prefix="/admin")
 app.include_router(auth.auth_router, tags=["auth"], prefix="/auth")
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
